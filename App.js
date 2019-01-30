a25dd6804ac62b160361e4e4541fbde625d344a8
foll <folleah5@gmail.com>
2019-01-30 19:59:45 +0300

    Added token to App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 const Telegraf = require('telegraf');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf('581665381:AAEUAFKyV-Es3H-n2kkxxaaGMN1W7CB7fz4');
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
