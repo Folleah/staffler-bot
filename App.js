5580d635c0f835b71aa7dcc2e3bb7f64e10a70f1
foll <folleah5@gmail.com>
2019-02-06 15:10:10 +0300

    Upd

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ bot.on('new_chat_members', (ctx) => {
 
 bot.on('message', (ctx) => {
   if (!isWhois(ctx.message.text) && isUserJoinedRecently(ctx.message.from.id)) {
-    console.log(ctx.message.entities);
+    console.log(ctx.message.entities !== null);
     if (ctx.message.entities !== null || ctx.message.photo !== null) {
 
       // telegram.kickChatMember(ctx.message.chat.id, ctx.message.from.id);
