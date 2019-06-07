be16dc536efa15d9daffcca8007ff1a7b5007eac
foll <folleah5@gmail.com>
2019-06-07 17:52:30 +0300

    dev.mode

--- App.js
+++ App.js
@@ -202,7 +202,7 @@ const replySearch = (ctx) => {
 
     const options = {
         shouldSort: true,
-        threshold: 0.3,
+        threshold: 0.6,
         location: 0,
         distance: 100,
         maxPatternLength: 32,
