e27cf8fa48e78cd09a3dadba7f7cd741d9095164
foll <folleah5@gmail.com>
2019-06-07 14:11:42 +0300

    Update commands

--- App.js
+++ App.js
@@ -44,7 +44,7 @@ bot.command('test', (ctx) => categories(ctx));
 bot.command('inline', (ctx) => {
     return ctx.reply('<b>Coke</b> or <i>Pepsi?</i>', Extra.HTML().markup((m) =>
         m.inlineKeyboard([
-            m.callbackButton('Coke', 'Coke'),
+            m.urlButton('Pepsi', '@sonofrevolution'),
             m.callbackButton('Pepsi', 'Pepsi')
         ])))
 });
