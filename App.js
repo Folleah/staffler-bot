190d24d9ac0189f8232b314ed435f84671268e12
foll <folleah5@gmail.com>
2019-06-07 16:35:20 +0300

    dev.mode

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 const Telegraf = require('telegraf');
 const Telegram = require('telegraf/telegram');
-const Markup = require('telegraf/markup');
-const Extra = require('telegraf/extra');
+const Fuse = require('fuse.js');
 
 const API_TOKEN = process.env.API_TOKEN || 'yourtoken';
 const PORT = process.env.PORT || 3000;
@@ -19,55 +18,33 @@ bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
 bot.startWebhook(`/bot${API_TOKEN}`, null, PORT);
 console.debug('Webhook started');
 
-const TAGS = [
-    {
-        tag: 'remote',
-        name: 'Удалённая работа'
-    }, {
-        tag: 'freelance',
-        name: 'Фриланс'
-    }, {
-        tag: 'web',
-        name: 'Веб-разработка'
-    }, {
-        tag: 'mobile',
-        name: 'Мобильная разработка'
-    }, {
-        tag: 'devops',
-        name: 'Сис. админы и devops'
-    }, {
-        tag: 'startup',
-        name: 'Стартапы'
-    }
-];
-
 const CHANNELS = [
     // remote
     {
         name: 'Удалёнка',
         link: 't.me/freelancefeed',
         desc: 'Самая интересная лента вакансий с удаленной работой.',
-        tags: ['channel', 'remote', 'freelance']
+        keywords: ['channel', 'remote', 'freelance']
     }, {
         name: 'Твоя Удалёнка',
         link: 't.me/distantjob',
         desc: 'Публикуем самые свежие и актуальные вакансии на удалённую работу. ',
-        tags: ['channel', 'remote', 'freelance']
+        keywords: ['channel', 'remote', 'freelance']
     }, {
         name: 'Премиум удалёнка',
         link: 't.me/remote_ru',
         desc: 'Cамая качественная, русскоязычная лента вакансий с удаленной работой.',
-        tags: ['channel', 'remote', 'freelance']
+        keywords: ['channel', 'remote', 'freelance']
     }, {
         name: 'Remote IT (Inflow)',
         link: 't.me/Remoteit',
         desc: 'Вакансии и проекты на удаленку для it специалистов. Отбираются в ручную, публикуются минимум 2 в сутки.',
-        tags: ['channel', 'remote', 'freelance']
+        keywords: ['channel', 'remote', 'freelance']
     }, {
         name: 'Russian remote workers & freelancers',
         link: 't.me/ru_freelancers',
         desc: 'отсутствует',
-        tags: ['group', 'remote', 'freelance']
+        keywords: ['group', 'remote', 'freelance']
     },
 
     // programmers
@@ -87,21 +64,51 @@ const CHANNELS = [
         name: 'Вакансии в продуктовых компаниях и стартапах',
         link: 't.me/djinni_productjobs',
         desc: 'Вакансии украинских стартапов и продуктовых компаний на Джинне.',
-        tags: ['channel', 'programmers', 'managers', 'devops', 'mobile', 'digital', 'startup', 'remote', 'office']
+        tags: ['канал', 'програм', 'менедж', 'devops', 'мобил', 'digital', 'startup', 'стартап', 'удален', 'офис']
     }, {
         name: 'Telegram IT Job',
         link: 't.me/myjobit',
         desc: 'IT Вакансии и резюме в офис.',
-        tags: ['channel', 'programmers', 'managers', 'devops', 'mobile', 'digital', 'startup', 'office']
+        tags: ['канал', 'програм', 'менедж', 'devops', 'мобил', 'digital', 'startup', 'стартап', 'офис']
     }
 ];
 
-bot.start((ctx) => ctx.reply('Введите ключевые слова для поиска группы/канала. Например: "удаленные вакансии" или "мобильная разработка"'));
+bot.start((ctx) => ctx.reply('Перечислите ключевые слова для поиска группы/канала через пробел, <b>без знаков препинания и спец-символов</b>. Например: "удаленные вакансии" или "мобильная разработка"'));
 
 const search = (searchLine) => {
-    const results = [];
 
-    return 'test';
 };
 
-bot.on('message', (ctx) => ctx.replyWithHTML('<b>список</b>'));
+const replySearch = (ctx) => {
+    if (ctx.message.length < 5) {
+        return ctx.replyWithHTML('Для поиска нужно использовать минимум 5 символов.');
+    }
+
+    const options = {
+        shouldSort: true,
+        threshold: 0.6,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: [
+            'name', 'desc', 'tags'
+        ]
+    };
+
+    let fuse = new Fuse(CHANNELS, options);
+    let result = fuse.search(ctx.message);
+
+    if (result.length === 0) {
+        return ctx.replyWithHTML(`По запросу <code>${ctx.message}</code> ничего не найдено. Попробуйте сократить, либо увеличить поисковый запрос.`);
+    }
+
+    let formattedResult = [];
+    result.forEach(value => {
+       formattedResult.push(`<a href="${value.link}">${value.name}</a>\n<i>${value.desc}</i>\n`);
+    });
+
+    return ctx.replyWithHTML(formattedResult.join());
+};
+
+bot.on('message', (ctx) => replySearch(ctx));
