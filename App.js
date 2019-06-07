c553824b44af75fa7596eca0ab73f3cd5c261246
foll <folleah5@gmail.com>
2019-06-07 14:55:55 +0300

    dev.mode

--- App.js
+++ App.js
@@ -16,7 +16,14 @@ console.debug('Bot initiated.');
 
 const CATEGORY_LIST = [
     ['Удалённые вакансии', 'remotes', [
-        'RemoteOK', ''
+        'Удалёнка', 't.me/freelancefeed',
+        'Твоя Удалёнка', 't.me/distantjob',
+        'Премиум удалёнка', 't.me/remote_ru',
+        'Remote IT (Inflow)', 't.me/Remoteit',
+        'Russian remote workers & freelancers', 't.me/ru_freelancers'
+    ]],
+    ['Менеджмент', 'managers', [
+
     ]],
     ['IT/Digital', 'it-digital', [
         'Ищу веб-разработчика', 't.me/fordev',
